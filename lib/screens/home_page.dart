429f464e76237cbfa0f7244eab5e0b7b1612535e
Sai <srinivassaichava@gmai.com>
2023-08-30 20:52:00 +0530

    Creation of Home and Appointment Pages

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:hosp_test/utils/config.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // Config.init(context);
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 15,
+        ),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Sai',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    child: CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage('assets/profile.jpg'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
